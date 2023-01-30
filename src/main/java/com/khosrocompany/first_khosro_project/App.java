2c2e0385a921328be19da2b2712429079424514a
khosro <khosro.hengaam@gmail.com>
2023-01-31 00:01:49 +0100

    added initial commit

---
+++ src/main/java/com/khosrocompany/first_khosro_project/App.java
@@ -0,0 +1,13 @@
+package com.khosrocompany.first_khosro_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
